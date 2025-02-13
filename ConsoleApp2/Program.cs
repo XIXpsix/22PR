65488d10e839277cc9805bdbdf9019aa95d16522
XIXpsix <lebedevdima902@gmail.com>
2025-02-13 11:22:35 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{      /// <summary>
+     /// 2. Создать объекты класса:с использованием конструктора без параметров и присваивания значений полям объекта;с явной инициализацией;с использованием конструктора
+     /// с двумя параметрами, введенными с клавиатуры.
+     /// </summary>
+     class Program
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+            calculate.material = 5;
+            calculate.material2 = 10;
+            Console.WriteLine();
+            Calculate calculate = new Calculate(4.5, 14.2);
+            Console.WriteLine(calculate);
+            Console.WriteLine();
+        }
+    }
+}
